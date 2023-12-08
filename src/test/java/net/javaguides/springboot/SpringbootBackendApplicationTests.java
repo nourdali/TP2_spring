ce2e26be6909d55ef996d7e3432e72bfe9453c80
Nour daly <nourhouda.dali01@gmail.com>
2023-12-08 23:12:47 +0100

    first

---
+++ src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SpringbootBackendApplicationTests.class)
+class SpringbootBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
